b1673d03e8cf21fba5e614adaa66ae2402388745
Rex16860 <dwireza1002@gmail.com>
2024-06-01 06:57:03 +0700

    Package

--- gempaTerkini/__init__.py
+++ gempaTerkini/__init__.py
@@ -1,3 +1,6 @@
+import requests
+
+
 def data_extract():
     """
     dateTime: date: 31 Mei 2024, time: 05:54:38 WIB
@@ -8,24 +11,39 @@ def data_extract():
     keterangan: tidak berpotensi TSUNAMI
     :return:
     """
-    data_result = {
-        'dateTime': {'date': '31 Mei 2024', 'time': '05:54:38 WIB'},
-        'magnitude': 5.4,
-        'kedalaman': '88 km',
-        'location': {'LU': 1.48, 'BT': 134.01},
-        'pusat': '139 km BaratLaut PULAUKARATUNG-SULUT',
-        'keterangan': 'tidak berpotensi TSUNAMI',
-    }
-    return data_result
+
+    try:
+        r = requests.get("https://www.bmkg.go.id/")
+    except Exception as e:
+        print(e)
+        return None
+
+    if r.status_code == 200:
+        print(r.text)
+        data_result = {
+            'dateTime': {'date': '31 Mei 2024', 'time': '05:54:38 WIB'},
+            'magnitude': 5.4,
+            'kedalaman': '88 km',
+            'location': {'LU': 1.48, 'BT': 134.01},
+            'pusat': '139 km BaratLaut PULAUKARATUNG-SULUT',
+            'keterangan': 'tidak berpotensi TSUNAMI',
+        }
+        return data_result
+    else:
+        return None
 
 
 def show_data(datas):
-    print(f'DateTime = {datas['dateTime']['date']}, {datas['dateTime']['time']}')
-    print(f'Magnitude = {datas['magnitude']}')
-    print(f'kedalaman = {datas['kedalaman']}')
-    print(f'location = {datas['location']['LU']}LU - {datas['location']['BT']}BT')
-    print(f'pusat = {datas['pusat']}')
-    print(f'keterangan = {datas['keterangan']}')
+    if datas is None:
+        print('data tidak ditemukan')
+    else:
+        print(f'DateTime = {datas['dateTime']['date']}, {datas['dateTime']['time']}')
+        print(f'Magnitude = {datas['magnitude']}')
+        print(f'kedalaman = {datas['kedalaman']}')
+        print(f'location = {datas['location']['LU']}LU - {datas['location']['BT']}BT')
+        print(f'pusat = {datas['pusat']}')
+        print(f'keterangan = {datas['keterangan']}')
+
 
 
 if __name__ == '__main__':
