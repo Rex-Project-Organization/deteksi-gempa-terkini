24b6b1c080d6287224d6a1dee0cfa1b13846944a
Rex16860 <dwireza1002@gmail.com>
2024-06-01 11:35:22 +0700

    gempaterkini with beautifulsoap

--- gempaTerkini/__init__.py
+++ gempaTerkini/__init__.py
@@ -1,4 +1,5 @@
 import requests
+from bs4 import BeautifulSoup
 
 
 def data_extract():
@@ -12,21 +13,66 @@ def data_extract():
     :return:
     """
 
+    # catch error
     try:
-        r = requests.get("https://www.bmkg.go.id/")
+        # get url
+        content = requests.get("https://www.bmkg.go.id/")
     except Exception as e:
         print(e)
         return None
 
-    if r.status_code == 200:
-        print(r.text)
+    if content.status_code == 200:
+
+        # create BeautifulSoup object
+        soup = BeautifulSoup(content.text, 'html.parser')
+
+        # find span tag
+        result = soup.find('span', {'class': 'waktu'})
+        date = result.text.split(', ')[0]
+        time = result.text.split(', ')[1]
+
+        # find list tag in div tag
+        result = soup.find('div', {'class': 'col-md-6 col-xs-6 gempabumi-detail no-padding'})
+        result = result.findChildren('li')
+
+        result_list = []
+
+        # substain result_list value
+        for i in range(len(result)):
+            result_list.append(result[i].text)
+
+        # remove not used value
+        result_list.pop()
+
+        # var
+        magnitude = ''
+        kedalaman = ''
+        location = ''
+        pusat = ''
+        keterangan = ''
+
+        # substain value to var
+        i = 1
+        if i < len(result_list):
+            magnitude = result_list[i]
+            i += 1
+            kedalaman = result_list[i]
+            i += 1
+
+            location = result_list[i]
+            i += 1
+            pusat = result_list[i]
+            i += 1
+            keterangan = result_list[i]
+            i += 1
+
         data_result = {
-            'dateTime': {'date': '31 Mei 2024', 'time': '05:54:38 WIB'},
-            'magnitude': 5.4,
-            'kedalaman': '88 km',
-            'location': {'LU': 1.48, 'BT': 134.01},
-            'pusat': '139 km BaratLaut PULAUKARATUNG-SULUT',
-            'keterangan': 'tidak berpotensi TSUNAMI',
+            'dateTime': {'date': date, 'time': time},
+            'magnitude': magnitude,
+            'kedalaman': kedalaman,
+            'location': location,
+            'pusat': pusat,
+            'keterangan': keterangan,
         }
         return data_result
     else:
@@ -34,16 +80,18 @@ def data_extract():
 
 
 def show_data(datas):
+    # error output
     if datas is None:
         print('data tidak ditemukan')
+        return
     else:
-        print(f'DateTime = {datas['dateTime']['date']}, {datas['dateTime']['time']}')
+        print(f'Date = {datas['dateTime']['date']}')
+        print(f'Time = {datas['dateTime']['time']}')
         print(f'Magnitude = {datas['magnitude']}')
         print(f'kedalaman = {datas['kedalaman']}')
-        print(f'location = {datas['location']['LU']}LU - {datas['location']['BT']}BT')
+        print(f'location = {datas['location']}')
         print(f'pusat = {datas['pusat']}')
         print(f'keterangan = {datas['keterangan']}')
-
 
 
 if __name__ == '__main__':
